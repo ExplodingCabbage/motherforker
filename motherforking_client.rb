1e23049c6b9f16c23f0e72d1b372fe2d45090893
Mark Amery <markamery@btinternet.com>
2014-12-04 17:10:19 +0000

    Skeleton of what needs doing

--- motherforking_client.rb
+++ motherforking_client.rb
@@ -1,5 +1,5 @@
 class MotherforkingClient
-  def initialize (login: nil, password: nil, access_token: nil)
+  def initialize(login: nil, password: nil, access_token: nil)
     if login && password
       @octokit_client = Octokit::Client.new(
         login: login,
@@ -15,5 +15,50 @@ class MotherforkingClient
     
     @user = @octokit_client.user
     @user.login
+  end
+  
+  def motherfork(old_repo)
+    new_repo = fork(old_repo)
+    copy_features_settings(old_repo, new_repo)
+    copy_labels(old_repo, new_repo)
+    copy_issues(old_repo, new_repo)
+    copy_pull_requests(old_repo, new_repo)
+    copy_wikis(old_repo, new_repo)
+    make_old_owner_a_collaborator(old_repo, new_repo)
+    invite_old_collaborators(old_repo, new_repo)
+  end
+  
+  private
+  
+  def fork(repo)
+    @octokit_client.fork(repo)[:full_name]
+  end
+  
+  def copy_features_settings(old_repo, new_repo)
+    raise "implement me"
+  end
+  
+  def copy_labels(old_repo, new_repo)
+    raise "implement me"
+  end
+  
+  def copy_issues(old_repo, new_repo)
+    raise "implement me"
+  end
+  
+  def copy_pull_requests(old_repo, new_repo)
+    raise "implement me"
+  end
+  
+  def copy_wikis(old_repo)
+    raise "implement me"
+  end
+  
+  def make_old_owner_a_collaborator(old_repo, new_repo)
+    raise "implement me"
+  end
+  
+  def invite_old_collaborators(old_repo, new_repo)
+    raise "implement me"
   end
 end
